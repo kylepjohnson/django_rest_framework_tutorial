2a026ab039f67e8dba45b8c5615763fbb5fe9506
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-03-14 12:46:55 -0400

    through pg 4 of tutorial

---
+++ tutorial/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf.urls import include
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tutorial.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+
+
+
+
+urlpatterns = [
+    url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
